d78afce6065db59de136b7ecd32273cce83f8540
Albert Babaiani <babaianialbert@gmail.com>
2026-03-10 14:54:28 +0400

    init project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductContent } from './components/product-content/product-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductContent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('e-commerce-sneakers-page');
