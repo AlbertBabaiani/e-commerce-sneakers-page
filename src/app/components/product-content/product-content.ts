dd68f93b05644200e27bed7822a5a058b6e57100
Albert Babaiani <babaianialbert@gmail.com>
2026-03-10 15:26:08 +0400

    add quantity control

--- src/app/components/product-content/product-content.ts
+++ src/app/components/product-content/product-content.ts
@@ -11,4 +11,17 @@ export class ProductContent {
   discount = signal<number>(0.5);
   original_price = signal<number>(250);
   current_price = computed(() => this.original_price() * (1 - this.discount()));
+
+  // Quantity Start
+
+  selected_quantity = signal<number>(0);
+
+  minusQuantity(): void {
+    this.selected_quantity.update((q) => q - 1);
+  }
+
+  plusQuantity(): void {
+    this.selected_quantity.update((q) => q + 1);
+  }
+  // Quantity End
 }
