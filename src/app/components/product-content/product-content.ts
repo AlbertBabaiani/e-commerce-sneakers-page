6d522b9027ce98fb9607c3168910980b5a818fba
Albert Babaiani <babaianialbert@gmail.com>
2026-03-10 15:13:15 +0400

    add price display

--- src/app/components/product-content/product-content.ts
+++ src/app/components/product-content/product-content.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, PercentPipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-product-content',
-  imports: [],
+  selector: 'section[app-product-content]',
+  imports: [CurrencyPipe, PercentPipe],
   templateUrl: './product-content.html',
   styleUrl: './product-content.scss',
 })
 export class ProductContent {
-
+  discount = signal<number>(0.5);
+  original_price = signal<number>(250);
+  current_price = computed(() => this.original_price() * (1 - this.discount()));
 }
