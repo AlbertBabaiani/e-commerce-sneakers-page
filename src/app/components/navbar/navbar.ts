602cafcd4f1a8c6b57e4816d02cd073227831674
Albert Babaiani <babaianialbert@gmail.com>
2026-03-19 12:46:48 +0400

    add dekstop navigation

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,5 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, input, output, signal } from '@angular/core';
+import { Component, HostListener, input, output, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -12,6 +12,13 @@ export class Navbar {
   links = ['collections', 'men', 'women', 'about', 'contact'];
 
   isOpened = signal<boolean>(false);
+
+  isMobile = signal<boolean>(window.innerWidth < 1440);
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobile.set(event.target.innerWidth < 1440);
+  }
 
   toggleNavBar(): void {
     this.isOpened.update((v) => !v);
