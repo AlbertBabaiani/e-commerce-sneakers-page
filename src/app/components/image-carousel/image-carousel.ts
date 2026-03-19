557660b6a9e9d4566d2a8c5ae14d7a1170b963f6
Albert Babaiani <babaianialbert@gmail.com>
2026-03-19 11:32:54 +0400

    add carousel animation

--- src/app/components/image-carousel/image-carousel.ts
+++ src/app/components/image-carousel/image-carousel.ts
@@ -1,29 +1,28 @@
+// image-carousel.ts
 import { Component, computed, input, signal } from '@angular/core';
 import { Product } from '../../shared/productModel';
 
 @Component({
   selector: 'section[app-image-carousel]',
-  imports: [],
   templateUrl: './image-carousel.html',
   styleUrl: './image-carousel.scss',
 })
 export class ImageCarousel {
   product = input.required<Product>();
-
   currentSlideIndex = signal(0);
 
-  currentImgUrl = computed(() => this.product().images[this.currentSlideIndex()].full);
-
   previous(): void {
-    this.currentSlideIndex.update((index) =>
-      index === 0 ? this.product().images.length - 1 : index - 1,
-    );
+    this.currentSlideIndex.update((index) => {
+      const newIndex = index === 0 ? this.product().images.length - 1 : index - 1;
+      return newIndex;
+    });
   }
 
   next(): void {
-    this.currentSlideIndex.update((index) =>
-      index === this.product().images.length - 1 ? 0 : index + 1,
-    );
+    this.currentSlideIndex.update((index) => {
+      const newIndex = index === this.product().images.length - 1 ? 0 : index + 1;
+      return newIndex;
+    });
   }
 
   setImageSlide(index: number) {
